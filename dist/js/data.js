23dce9a8ec85b325364e356052ec6f011966f525
noah112k <50031627+noah112k@users.noreply.github.com>
2019-08-20 18:07:24 -0700

    Basic responsiveness and polishing js

---
+++ dist/js/data.js
@@ -0,0 +1,23 @@
+let quote1 =
+  '"The most fucked up joke life can play on you is letting you meet the right person at the wrong time"';
+let quote2 =
+  '"I think the saddest people always try their hardest to make people happy. Because they know what it feels like to feel absolutely worthless and they don\'t want anyone else to feel like that."';
+let quote3 =
+  "\"When you're happy you enjoy the music. When you're sad you understand the lyrics.\"";
+let quote4 =
+  '"Everyone you meet always asks if you have a career, are married, or own a house as if life was some kind of grocery list. But no one ever asks you if you are happy."';
+let quote5 =
+  "\"I'm not upset that you lied to me, I'm upset that from now on I can't believe you.\"";
+let img1 = "Image 1";
+let img2 = "Image 2";
+let img3 = "Image 3";
+let img4 = "Image 4";
+let img5 = "Image 5";
+let auth1 = "-Unknown";
+let auth2 = "-Robin Williams";
+let auth3 = "-Frank Ocean";
+let auth4 = "-Heath Ledger";
+let auth5 = "-Friedrich Nietzsche";
+let quotes = [quote1, quote2, quote3, quote4, quote5];
+let images = [img1, img2, img3, img4, img5];
+let authors = [auth1, auth2, auth3, auth4, auth5];
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,39 +1,32 @@
-let quote1 =
-  '"The most fucked up joke life can play on you is letting you meet the right person at the wrong time"';
-let quote2 =
-  '"I think the saddest people always try their hardest to make people happy. Because they know what it feels like to feel absolutely worthless and they don\'t want anyone else to feel like that."';
-let quote3 =
-  "\"When you're happy you enjoy the music. When you're sad you understand the lyrics.\"";
-let quote4 = "Test 4";
-let quote5 = "Test 5";
-let img1 = "Image 1";
-let img2 = "Image 2";
-let img3 = "Image 3";
-let img4 = "Image 4";
-let img5 = "Image 5";
-let auth1 = "-Unknown";
-let auth2 = "-Robin Williams";
-let auth3 = "-Frank Ocean";
-let auth4 = "Author 4";
-let auth5 = "Author 5";
-let quotes = [quote1, quote2, quote3, quote4, quote5];
-let images = [img1, img2, img3, img4, img5];
-let authors = [auth1, auth2, auth3, auth4, auth5];
-
 let button = document
   .querySelector(".button")
   .addEventListener("click", changeQuote);
+let currentQuote = document.querySelector(".quote");
+let currentAuth = document.querySelector(".author");
 function changeQuote() {
-  let currentQuote = document.querySelector(".quote");
-  let currentAuth = document.querySelector(".author");
   currentQuote.classList.add("fade-out");
   currentAuth.classList.add("fade-out");
   setTimeout(replaceQuote, 2000);
 }
+
+let prevNum = -1;
+
 function replaceQuote() {
-  let num = Math.round(Math.random() * (quotes.length - 1));
-  let currentQuote = document.querySelector(".quote");
-  let currentAuth = document.querySelector(".author");
+  let num = prevNum;
+  if (num >= 0) {
+    if (quotes[num].length > 150) {
+      currentQuote.classList.remove("long-quote");
+      currentQuote.classList.add("quote");
+    }
+  }
+  while (num == prevNum) {
+    num = Math.round(Math.random() * (quotes.length - 1));
+  }
+  prevNum = num;
+  if (quotes[num].length > 150) {
+    currentQuote.classList.add("long-quote");
+    currentQuote.classList.remove("quote");
+  }
   currentQuote.innerHTML = quotes[num];
   currentAuth.innerHTML = authors[num];
   currentQuote.classList.remove("fade-out");
